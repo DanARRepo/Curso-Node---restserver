4d474a83422a86cf453fd3055edd4ae7052542f4
Daniel_Amaya <daniel.amaya@sinapsis.global>
2023-01-28 16:21:29 -0500

    handle rejections

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,72 +3,93 @@ const bcryptjs = require('bcryptjs');
 
 const User = require('../models/user');
 
-const userGet = async( req, res = response ) => {
-    const { limit = 5, from = 0 } = req.query;
-    const query = { state: true };
-    
-    const [ total, users ] = await Promise.all([
-        User.countDocuments(query),
-        User.find(query)
-            .limit(Number(limit))
-            .skip(Number(from))
-    ]).catch( () => {
-        console.log('Promise rejected');
-    });
+const userGet = async (req, res = response) => {
+    try {
+        const { limit = 5, from = 0 } = req.query;
+        const query = { state: true };
 
-    res.json({
-        total,
-        users
-    });
+        const [total, users] = await Promise.all([
+            User.countDocuments(query),
+            User.find(query)
+                .limit(Number(limit))
+                .skip(Number(from))
+        ]).catch(() => {
+            console.log('Promise rejected');
+        });
+
+        res.json({
+            total,
+            users
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
-const userPost = async(req, res = response) => {
-    const { name, email, password, role } = req.body;
-    const user = new User({ name, email, password, role });
-    
-    // Encriptar pass
-    const salt = bcryptjs.genSaltSync();
-    user.password = bcryptjs.hashSync( password, salt );
+const userPost = async (req, res = response) => {
+    try {
+        const { name, email, password, role } = req.body;
+        const user = new User({ name, email, password, role });
 
-    // Guardar en BD
-    await user.save();
-
-    res.json({
-        user
-    });
-}
-
-const userPut = async(req, res = response) => {
-    const { id } = req.params;
-    const { _id, password, google, ...all } = req.body;
-
-    if ( password ) {
+        // Encriptar pass
         const salt = bcryptjs.genSaltSync();
-        all.password = bcryptjs.hashSync( password, salt );
+        user.password = bcryptjs.hashSync(password, salt);
+
+        // Guardar en BD
+        await user.save();
+
+        res.json({
+            user
+        });
+    } catch (err) {
+        next(err);
     }
 
-    const userDB = await User.findByIdAndUpdate( id, all );
+}
 
-    res.json( userDB );
+const userPut = async (req, res = response) => {
+    try {
+        const { id } = req.params;
+        const { _id, password, google, ...all } = req.body;
+
+        if (password) {
+            const salt = bcryptjs.genSaltSync();
+            all.password = bcryptjs.hashSync(password, salt);
+        }
+
+        const userDB = await User.findByIdAndUpdate(id, all);
+
+        res.json(userDB);
+    } catch (err) {
+        next(err);
+    }
 }
 
 const userPatch = (req, res = response) => {
-    res.json({
-        msg: 'patch API - Controller'
-    });
+    try {
+        res.json({
+            msg: 'patch API - Controller'
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
-const userDelete = async(req, res = response) => {
-    const { id } = req.params;
+const userDelete = async (req, res = response) => {
+    try {
+        const { id } = req.params;
 
-    // Para borrarlo fisicamente 
-    // const user = await User.findByIdAndDelete(id);
+        // Para borrarlo fisicamente 
+        // const user = await User.findByIdAndDelete(id);
 
-    const user = await User.findByIdAndUpdate( id, { state: false } );
+        const user = await User.findByIdAndUpdate(id, { state: false });
 
-    res.json({
-        user
-    });
+        res.json({
+            user
+        });
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {
